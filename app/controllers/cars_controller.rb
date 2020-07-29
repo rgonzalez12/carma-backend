b3e27b61e50e69692bebde84c5bec9dfec5a8735
rgonzalez12 <raymondalexander.gonzalez@gmail.com>
2020-07-29 16:25:57 -0600

    added route for api endpoint. added private methods for cars controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,14 @@
 class CarsController < ApplicationController
+
+
+    prviate 
+
+    def set_car
+        @car = Car.find(params[:id])
+    end
+
+    def car_params
+        params.require(:car).permit()
+    end
+
 end
